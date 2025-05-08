40d8f650df1ba169d0853156763a5760d2a0e451
ilham101106 <pesatoffical321@gmail.com>
2025-05-08 15:44:46 +0700

    function untuk menjalankan current dan previous

--- linklist.cpp
+++ linklist.cpp
@@ -44,8 +44,17 @@ public:
 node *previous = start;
 node *current = start;
 
-
-
-
+while ((current != NULL) && (nim >= current->noMhs)){
+    if (nim== current->noMhs){
+        cout << "\nDuplikasi noMhs tidak diijinkan\n";
+        return;
     }
+    previous = current;
+    current= current->next;
+}
+
+nodebaru->next = current;
+previous->next = nodebaru;
+
+}
 }
